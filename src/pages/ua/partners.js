601d34b329c46b3e2431a6b4eeb82938bd9cb971
Volodymyr Danylko <danylko91@gmail.com>
2022-09-13 11:27:10 +0300

    Peplaced fetch

--- src/pages/ua/partners.js
+++ src/pages/ua/partners.js
@@ -1,6 +1,31 @@
 import React from "react"
+import useSWR from "swr"
 
 const index = () => {
+  const fetchWithToken = async (url, token) => {
+    const headers = new Headers()
+
+    headers.append("Content-Type", "application/json")
+    headers.append("Accept", "application/json")
+    headers.append("Authorization", `Bearer ${token}`)
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers,
+    })
+
+    return response
+  }
+
+  const { data: collections } = useSWR(
+    [
+      "https://api.intercom.io/help_center/collections",
+      "dG9rOjFkOGIzODRmXzEyY2ZfNDRjMV85NDg3Xzk3NDFjYTE3OGZmMjoxOjA=",
+    ],
+    fetchWithToken
+  )
+  console.log(collections)
+  console.log(5)
   return <div>UA PArtners page</div>
 }
 
