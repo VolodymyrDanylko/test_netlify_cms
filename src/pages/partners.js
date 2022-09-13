4eef7d727110703a45a07c351dcc3af729aea19c
Volodymyr Danylko <danylko91@gmail.com>
2022-09-13 11:38:03 +0300

    Peplaced fetch

--- src/pages/partners.js
+++ src/pages/partners.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import useSWR from "swr"
 import { graphql } from "gatsby"
 import { remark } from "remark"
 import remarkHTML from "remark-html"
@@ -12,6 +13,31 @@ const toHTML = value => remark().use(remarkHTML).processSync(value).toString()
 const PartnersPage = ({ data, location }) => {
   const frontmatter = data.markdownRemark.frontmatter
   const { sectionFAQ, path } = frontmatter
+
+  const fetchWithToken = async (url, token) => {
+    const headers = new Headers()
+
+    headers.append("Content-Type", "application/json")
+    headers.append("Accept", "application/json")
+    headers.append("Authorization", `Bearer ${token}`)
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers,
+    })
+
+    return response
+  }
+
+  const { data: collections } = useSWR(
+    [
+      "https://api.intercom.io/help_center/collections",
+      "dG9rOjFkOGIzODRmXzEyY2ZfNDRjMV85NDg3Xzk3NDFjYTE3OGZmMjoxOjA=",
+    ],
+    fetchWithToken
+  )
+  console.log(collections)
+  console.log(5)
 
   return (
     <Layout location={location} title={path}>
@@ -45,7 +71,7 @@ const PartnersPage = ({ data, location }) => {
 export default PartnersPage
 
 export const pageQuery = graphql`
-  query($path: String!) {
+  query ($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       frontmatter {
         path
